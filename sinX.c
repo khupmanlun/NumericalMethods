04d1fdf8e9c2f7f36880bbe29701b56ef743ea41
khupmanlun <khupmanlun@rediffmail.com>
2018-04-23 18:58:40 +0530

    sine series sum

---
+++ sinX.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void){
+	int n,i;
+    float  sum,term,x;
+    printf("Type the value of angle: \t");
+    scanf("%f",&x);
+    x = (x * 3.141)/180;
+    term  = x;
+    sum  = x;
+    printf("\nType the number of terms: \t");
+    scanf("%d",&n);
+     for(i=1;i<n-1;i++){
+        term = (term  * (-1) * x * x)/((2*i)*(2*i+1));
+        sum+=term;
+    }
+    printf("The sum of the series till %d terms is %.3f",n,sum);
+}
